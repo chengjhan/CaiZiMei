06049d195d1c0a9028fcec5c47ee89f28bfcee11
chengjhan <chengjhan@gmail.com>
2017-07-20 11:43:50 +0800

    add SendMail.class's bean annotation

--- Admin/src/main/java/com/czmbeauty/common/mail/SendMail.java
+++ Admin/src/main/java/com/czmbeauty/common/mail/SendMail.java
@@ -1,15 +1,19 @@
 package com.czmbeauty.common.mail;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
 
+@Component(value = "sendMail")
 public class SendMail {
 
+	@Autowired
 	private MailSender mailSender;
 
-	public void setMailSender(MailSender mailSender) {
-		this.mailSender = mailSender;
-	}
+//	public void setMailSender(MailSender mailSender) {
+//		this.mailSender = mailSender;
+//	}
 
 	/**
 	 * 寄送 Email

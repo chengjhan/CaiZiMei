bf5e5e8b2b8ddaf2178acf281c394c1dfabf49b1
chengjhan <chengjhan@gmail.com>
2017-07-23 01:00:16 +0800

    修改sql

--- Admin/src/main/resources/sql/czmbeautydb_mysql.sql
+++ Admin/src/main/resources/sql/czmbeautydb_mysql.sql
@@ -112,4 +112,22 @@ create table franchisee (
 	foreign key (fr_ci_id) references city (ci_id)
 );
 
+-- admin
 insert into admin (ad_id, ad_username, ad_password, ad_salt, ad_lastname, ad_firstname, ad_email, ad_signup_time, ad_signin_number, ad_signin_ip, ad_signin_time, ad_update_info_time, ad_update_pwd_time, ad_status, ad_status_time) values (100, 'admin', '538f10610b9eda06f83d7d73332d2ed0', '37f3d447-fad6-4ec0-b5fb-6a8f05b60c77', null, null, 'admin@czmbeauty.com', now(), null, null, null, now(), now(), 1, now());
+
+-- country
+insert into country (co_iso, co_name, co_phonecode, co_rank) values ('TW', '台灣', '886', 1);
+insert into country (co_iso, co_name, co_phonecode, co_rank) values ('CN', '中國', '86', 2);
+
+-- state
+insert into state (st_co_id, st_name, st_rank) values (1, '台灣', 1);
+insert into state (st_co_id, st_name, st_rank) values (1, '澎湖', 2);
+insert into state (st_co_id, st_name, st_rank) values (2, '福建省', 1);
+insert into state (st_co_id, st_name, st_rank) values (2, '廣東省', 2);
+
+-- city
+insert into city (ci_co_id, ci_st_id, ci_name, ci_rank) values (1, 11, '台北市', 1);
+insert into city (ci_co_id, ci_st_id, ci_name, ci_rank) values (1, 11, '新北市', 2);
+
+-- clinic
+insert into clinic (cl_name, cl_eng_name, cl_localphone, cl_co_id, cl_st_id, cl_ci_id, cl_address, cl_latitude, cl_longitude, cl_url, cl_insert_time, cl_update_time, cl_status, cl_status_time) values ('曼星整形醫美診所', 'Twinkle Clinic', '02-27079333', 1, 11, 101, '大安區信義路四段58號3F-2', 25.033072, 121.545437, 'http://twinkle-clinic.tw/', now(), now(), '1', now());

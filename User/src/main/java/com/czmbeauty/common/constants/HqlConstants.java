cf5c962f7bb1c37cd04efaad8240f8954a92aa0f
chengjhan <chengjhan@gmail.com>
2017-09-08 23:29:00 +0800

    修改image getJSON方法

--- User/src/main/java/com/czmbeauty/common/constants/HqlConstants.java
+++ User/src/main/java/com/czmbeauty/common/constants/HqlConstants.java
@@ -10,10 +10,6 @@ public interface HqlConstants {
 	/** 搜尋開啟的據點 */
 	public static final String HQL_SELECT_OPEN_BASE = "from BaseBean where ba_status=1 order by ba_id asc";
 
-	// image
-	/** 類別流水號搜尋開啟的圖片 */
-	public static final String HQL_SELECT_OPEN_IMAGE_BY_CATEGORY = "from ImageBean where im_ca_id=:im_ca_id and im_status=1 order by im_rank asc";
-
 	// video
 	/** 搜尋開啟的相關影音 */
 	public static final String HQL_SELECT_OPEN_VIDEO_RELATED = "from VideoBean where vi_ca_id=5 and vi_status=1 order by vi_rank asc";

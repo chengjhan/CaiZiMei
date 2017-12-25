0e32b26deefbe662beeca5afb46b406e59c9dfae
chengjhan <chengjhan@gmail.com>
2017-12-25 14:48:44 +0800

    modify StopwatchInterceptor in User

--- User/src/main/java/com/czmbeauty/common/interceptor/StopwatchInterceptor.java
+++ User/src/main/java/com/czmbeauty/common/interceptor/StopwatchInterceptor.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,9 +19,13 @@ public class StopwatchInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 
+		HandlerMethod handlerMethod = (HandlerMethod) handler;
+		String handlerClassName = handlerMethod.getBeanType().getSimpleName();
+		String handlerMethodName = handlerMethod.getMethod().getName();
+
 		startTime = System.currentTimeMillis();
 
-		logger.info("time start...");
+		logger.info("(" + handlerClassName + "." + handlerMethodName + ") time start...");
 
 		return true;
 	}
@@ -35,9 +40,14 @@ public class StopwatchInterceptor implements HandlerInterceptor {
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e)
 			throws Exception {
 
+		HandlerMethod handlerMethod = (HandlerMethod) handler;
+		String handlerClassName = handlerMethod.getBeanType().getSimpleName();
+		String handlerMethodName = handlerMethod.getMethod().getName();
+
 		endTime = System.currentTimeMillis();
 
-		logger.info("time stop, time: " + (endTime - startTime) + "ms\n");
+		logger.info("(" + handlerClassName + "." + handlerMethodName + ") time stop, time: " + (endTime - startTime)
+				+ "ms\n");
 	}
 
 }
